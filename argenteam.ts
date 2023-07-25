70405b851623f789fa5d7976b294c29409d2a539
lndgalante <leo@phantom.app>
2023-07-25 12:37:11 -0300

    feat: add Argenteam subtitle getter function

---
+++ argenteam.ts
@@ -0,0 +1,117 @@
+import invariant from "tiny-invariant";
+import { match } from "ts-pattern";
+import { z } from "zod";
+
+const ARGENTEAM_BASE_URL = "http://argenteam.net/api/v1" as const;
+
+const argenteamApiEndpoints = {
+  search: (query: string) => {
+    return `${ARGENTEAM_BASE_URL}/search?q=${query}`;
+  },
+  tvShow: (id: number) => {
+    return `${ARGENTEAM_BASE_URL}/tvshow?id=${id}`;
+  },
+  episode: (id: number) => {
+    return `${ARGENTEAM_BASE_URL}/episode?id=${id}`;
+  },
+  movie: (id: number) => {
+    return `${ARGENTEAM_BASE_URL}/movie?id=${id}`;
+  },
+};
+
+const argenteamSearchResultSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  type: z.string(),
+  summary: z.string(),
+  imdb: z.string(),
+  poster: z.string(),
+});
+
+const argenteamSearchSchema = z.object({
+  results: z.array(argenteamSearchResultSchema),
+  total: z.number(),
+  offset: z.number(),
+});
+
+const argenteamResourceInfoSchema = z.object({
+  title: z.string(),
+  imdb: z.string(),
+  year: z.number(),
+  rating: z.number(),
+  runtime: z.number(),
+  alternativeTitle: z.string(),
+  country: z.string(),
+  poster: z.string(),
+  director: z.string(),
+  actors: z.string(),
+});
+
+const argenteamResourceSubtitlesSchema = z.object({
+  uri: z.string(),
+  count: z.number(),
+});
+
+const argenteamResourceReleaseSchema = z.object({
+  source: z.string(),
+  codec: z.string(),
+  team: z.string(),
+  tags: z.string(),
+  size: z.string(),
+  torrents: z.any(),
+  elinks: z.any(),
+  subtitles: z.array(argenteamResourceSubtitlesSchema),
+});
+
+const argenteamResourceSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  summary: z.string(),
+  info: argenteamResourceInfoSchema,
+  releases: z.array(argenteamResourceReleaseSchema),
+});
+
+export async function getArgenteamSubtitle(
+  imdbId: string,
+  releaseGroup: string,
+  quality: string,
+) {
+  // 1. Parse imdb id
+  const parsedImdbId = imdbId.replace("tt", "");
+
+  // 2. Get argenteam search results
+  const argenteamSearchEndpoint = argenteamApiEndpoints.search(parsedImdbId);
+  const searchResponse = await fetch(argenteamSearchEndpoint);
+  const rawSearchData = await searchResponse.json();
+
+  const { results } = argenteamSearchSchema.parse(rawSearchData);
+  invariant(results.length > 0, "There should be at least one result");
+
+  // 3. Get argenteam resource data
+  const { id, type } = results[0];
+
+  const argenteamResourceEndpoint = match(type)
+    .with("movie", () => argenteamApiEndpoints.movie(id))
+    .with("episode", () => argenteamApiEndpoints.episode(id))
+    .with("tvshow", () => argenteamApiEndpoints.tvShow(id))
+    .otherwise(() => {
+      throw new Error(`type ${type} not supported`);
+    });
+
+  const resourceResponse = await fetch(argenteamResourceEndpoint);
+  const rawResourceData = await resourceResponse.json();
+
+  // 4. Filter releases by release group and quality
+  const { releases } = argenteamResourceSchema.parse(rawResourceData);
+  const release = releases.find(
+    (release) => release.team === releaseGroup && release.tags === quality,
+  );
+  invariant(release, "Release should exist");
+
+  // 5. Get subtitle link
+  const { subtitles } = release;
+  invariant(subtitles.length > 0, "There should be at least one subtitle");
+
+  const subtitleLink = subtitles[0].uri;
+  return subtitleLink;
+}
---
+++ tests/getArgenteamSubtitle.test.ts
@@ -0,0 +1,21 @@
+import { describe, it } from "vitest";
+
+import { getArgenteamSubtitle } from "../argenteam";
+
+describe("getArgenteamSubtitle", () => {
+  it("should return a subtitle link giving a movie, release group and quality", async ({
+    expect,
+  }) => {
+    const subtitleLink = await getArgenteamSubtitle(
+      "tt0439572",
+      "RiGHTNOW",
+      "1080p",
+    );
+
+    expect(subtitleLink).toBe(
+      "https://argenteam.net/subtitles/90162/The.Flash.%282023%29.WEB-DL.H264.1080p-RiGHTNOW",
+    );
+  });
+
+  // TODO: Add more tests cases
+});
