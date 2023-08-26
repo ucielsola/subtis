973a11952fcbf7d23e4a377a30df232e29a5c5c6
lndgalante <lndgalante@gmail.com>
2023-08-26 20:52:51 -0300

    feat: Improve environment variable parsing for supabase

--- supabase.ts
+++ supabase.ts
@@ -1,16 +1,37 @@
 import "dotenv/config";
+
+import { z } from "zod";
 import { createClient } from "@supabase/supabase-js";
 
 // supabase db type definitions
 import { Database } from "./supabase-types";
 
-export type SupabaseClient = ReturnType<typeof getSupabaseClient>;
-
-function getSupabaseClient() {
+function getSupabaseEnvironmentVariables(): {
+  supabaseApiKey: string;
+  supabaseBaseUrl: string;
+} {
   const [supabaseApiKey, supabaseBaseUrl] = [
     process.env.SUPABASE_API_KEY,
     process.env.SUPABASE_BASE_URL,
-  ] as [string, string];
+  ];
+
+  const supabaseEnvVars = {
+    supabaseApiKey,
+    supabaseBaseUrl,
+  };
+
+  const schema = z.object({
+    supabaseApiKey: z.string(),
+    supabaseBaseUrl: z.string(),
+  });
+
+  return schema.parse(supabaseEnvVars);
+}
+
+export type SupabaseClient = ReturnType<typeof getSupabaseClient>;
+
+function getSupabaseClient() {
+  const { supabaseApiKey, supabaseBaseUrl } = getSupabaseEnvironmentVariables();
 
   return createClient<Database>(supabaseBaseUrl, supabaseApiKey, {
     auth: { persistSession: false },
