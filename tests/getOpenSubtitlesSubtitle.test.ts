ad2029d6e5dd9ae8d715f010c0267f20d29e808f
lndgalante <lndgalante@gmail.com>
2023-08-26 22:56:19 -0300

    test: Add extra unit test for open subtitles

--- tests/getOpenSubtitlesSubtitle.test.ts
+++ tests/getOpenSubtitlesSubtitle.test.ts
@@ -32,4 +32,31 @@ describe("getOpenSubtitlesSubtitle", () => {
         "meg-2-the-trench-1080p-yts-mx-opensubtitles",
     });
   });
+
+  it("should return a subtitle link giving a movie, release group and quality", async ({
+    expect,
+  }) => {
+    const movieData = {
+      name: "Junk Head",
+      searchableMovieName: "Junk Head (2017)",
+      year: 2017,
+      resolution: "1080p",
+      searchableSubDivXName: "YTS MX",
+      searchableArgenteamName: "YIFY",
+      searchableOpenSubtitlesName: "YTS.MX",
+      releaseGroup: "YTS-MX" as ReleaseGroupNames,
+    };
+
+    const subtitle = await getOpenSubtitlesSubtitle(movieData, 6848928);
+
+    expect(subtitle).toEqual({
+      fileExtension: "srt",
+      subtitleLink:
+        "https://www.opensubtitles.com/download/3E56B9ACBFB53063B1A115DA429C507CF50E230F959DEA8376DA9CC7FA93807D2F96C240CF9DCCDA1C32FB3632FD3B54DB3B8862E643DBFC0225D8D4216ACEB87F3DC79396F3551E1EE82965E8984F590EE075E4562EF490B34B872EA35B04DD38ACC11C502A768652450450D50B82D000AFDB71E5424A55DB363FC9F99C110864FEBEE3944E401C607E8C4F45708ADFD2191211303BDB3E819EF4B7678D87867F603319FFF55409547197F6FD489552629F88A2F5A254A859555AA5A3E7FCC82862977DA64296C706A39DF7D77053CAA5E4796E2499B6730F8FBFFE8FE79027F400409B4AD3792AB898D2BDCCDBB2E9E24E6BFF8C3DBD1FA13B79C0DAFE9A78D17FA4C299EF7327F1A358346B8DE620B6F0F866DA756D18FABC8C7FDE197F38A97FF4298252F940/subfile/Junk.Head.2017.1080p.BluRay.x264.AAC-%5BYTS.srt",
+      subtitleGroup: "OpenSubtitles",
+      subtitleSrtFileName: "junk-head-1080p-yts-mx-opensubtitles.srt",
+      subtitleCompressedFileName: "junk-head-1080p-yts-mx-opensubtitles.srt",
+      subtitleFileNameWithoutExtension: "junk-head-1080p-yts-mx-opensubtitles",
+    });
+  });
 });
