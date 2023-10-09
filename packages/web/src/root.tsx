c8ce88d098b4b300069a8d46355cbd3fee6b4107
lndgalante <leo@phantom.app>
2023-10-09 02:01:50 -0300

    feat: General web cleanup

--- packages/web/src/root.tsx
+++ packages/web/src/root.tsx
@@ -1,18 +1,13 @@
 import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 
+// components
 import { RouterHead } from './components/router-head/router-head';
 
+// styles
 import './global.css';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
-
   return (
     <QwikCityProvider>
       <head>
@@ -20,7 +15,7 @@ export default component$(() => {
         <link rel='manifest' href='/manifest.json' />
         <RouterHead />
       </head>
-      <body lang='en'>
+      <body lang='es'>
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
