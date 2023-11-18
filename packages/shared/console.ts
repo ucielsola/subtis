39628f3d5ce2be07fef244b11812ca268ea5f3f3
lndgalante <lndgalante@gmail.com>
2023-11-18 11:18:10 -0500

    feat: Add console table support for Bun

---
+++ packages/shared/console.ts
@@ -0,0 +1,17 @@
+// TODO: Remove this file when console.table is fully supported in Bun
+
+import { Console } from 'node:console'
+import { Transform } from 'node:stream'
+
+const ts = new Transform({ transform(chunk, _, cb) { cb(null, chunk) } })
+const logger = new Console({ stdout: ts })
+
+function getTable(data: any) {
+  logger.table(data)
+  const table = (ts.read() || '').toString()
+  // eslint-disable-next-line no-console
+  console.log(table)
+}
+
+// eslint-disable-next-line no-console
+console.table = getTable
