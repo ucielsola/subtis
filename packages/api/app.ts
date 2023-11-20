013e73368e0c7ecd2002a1d235b509fca953f3ae
lndgalante <lndgalante@gmail.com>
2023-11-20 12:48:13 -0500

    feat: Add movies endpoint. Add missing tests.

--- packages/api/app.ts
+++ packages/api/app.ts
@@ -3,6 +3,7 @@ import { cors } from '@elysiajs/cors'
 import { swagger } from '@elysiajs/swagger'
 
 // internals
+import { getMoviesFromMovieId } from './movies'
 import { getSubtitleFromFileName } from './subtitle'
 import { getSubtitlesFromMovieId } from './subtitles'
 
@@ -10,6 +11,9 @@ import { getSubtitlesFromMovieId } from './subtitles'
 export const app = new Elysia()
   .use(cors())
   .use(swagger({ path: '/v1/docs' }))
+  .post('/v1/movies', ({ set, body }) => getMoviesFromMovieId({ set, body }), {
+    body: t.Object({ movieName: t.String() }),
+  })
   .post('/v1/subtitle', ({ set, body }) => getSubtitleFromFileName({ set, body }), {
     body: t.Object({ fileName: t.String() }),
   })
---
+++ packages/api/movies.ts
@@ -0,0 +1,62 @@
+import { z } from 'zod'
+import invariant from 'tiny-invariant'
+import type { Context } from 'elysia'
+
+// shared
+import { getIsInvariantError, getParsedInvariantMessage } from 'shared/invariant'
+
+// db
+import { type Movie, supabase } from 'db'
+
+// types
+type ApiResponseError = { message: string }
+
+type CustomQuery = Pick<Movie, 'id' | 'name' | 'year'>[] | null | ApiResponseError
+
+// schemas
+const errorSchema = z.object({ status: z.number(), message: z.string() })
+
+// core
+export async function getMoviesFromMovieId({
+  set,
+  body,
+}: {
+  set: Context['set']
+  body: { movieName: string }
+}): Promise<CustomQuery> {
+  try {
+    // 1. Get fileName from body
+    const { movieName } = body
+
+    // 2. Get movies from database
+    const { data: movies } = await supabase
+      .from('Movies')
+      .select(
+        'id, name, year',
+      )
+      .ilike('name', `${movieName}%`)
+      .limit(10)
+
+    // 3. Throw error if movies is not found
+    invariant(movies && movies.length !== 0, JSON.stringify({ message: `Movies not found for query ${movieName}`, status: 404 }))
+
+    // 4. Return movies
+    return movies
+  }
+  catch (error) {
+    const nativeError = error as Error
+    const isInvariantError = getIsInvariantError(nativeError)
+
+    if (!isInvariantError) {
+      set.status = 500
+      return { message: nativeError.message }
+    }
+
+    const invariantMessage = getParsedInvariantMessage(nativeError)
+    const invariantError = JSON.parse(invariantMessage)
+    const { status, message } = errorSchema.parse(invariantError)
+
+    set.status = status
+    return { message }
+  }
+}
