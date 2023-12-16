0589b0514c50cd3311afe294f47651750afb515e
lndgalante <lndgalante@gmail.com>
2023-12-16 15:26:49 -0300

    fix: Codebase lint issues fixed

--- packages/cli/api.ts
+++ packages/cli/api.ts
@@ -7,5 +7,5 @@ const isProduction = Bun.env.NODE_ENV === 'production'
 export const apiClient = getApiClient({
   isProduction,
   apiBaseUrlProduction: isProduction ? '' : 'http://localhost:8080',
-  apiBaseUrlDevelopment: isProduction ? '' :'http://localhost:8080',
+  apiBaseUrlDevelopment: isProduction ? '' : 'http://localhost:8080',
 })
---
+++ packages/shared/tests/getMessageFromStatusCode.test.ts
@@ -0,0 +1,32 @@
+import { expect, test } from 'bun:test'
+
+// shared
+import { getMessageFromStatusCode } from 'shared/error-messages'
+
+test('returns correct message for 415', () => {
+  expect(getMessageFromStatusCode(415)).toEqual({
+    title: 'Extensión de archivo no soportada',
+    description: 'Prueba con formatos como MKV, MP4, o AVI',
+  })
+})
+
+test('returns correct message for 404', () => {
+  expect(getMessageFromStatusCode(404)).toEqual({
+    title: 'Subtitulo no encontrado',
+    description: 'Nos pondremos a buscarlo',
+  })
+})
+
+test('returns correct message for 500', () => {
+  expect(getMessageFromStatusCode(500)).toEqual({
+    title: 'Error Inesperado',
+    description: 'Estamos haciendo arreglos del servicio',
+  })
+})
+
+test('returns correct message for unknown status codes', () => {
+  expect(getMessageFromStatusCode(999)).toEqual({
+    title: 'Error desconocido',
+    description: 'Estamos haciendo arreglos del servicio',
+  })
+})
