07396bd8d9e597ac6d33a9444d348e52163bd6ba
lndgalante <lndgalante@gmail.com>
2023-12-16 14:44:28 -0300

    feat: Display log when app begins running

--- packages/api/app/app.ts
+++ packages/api/app/app.ts
@@ -1,3 +1,6 @@
 import { initializeElyisia } from './elysia'
 
 export const app = initializeElyisia()
+
+// eslint-disable-next-line no-console
+console.log(`🟢 Subtis API is running at https://${app.server?.hostname}:${app.server?.port}`)
