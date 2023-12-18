b4cc8d14d7d2742186c04c6c2c49695034401b3d
lndgalante <lndgalante@gmail.com>
2023-12-18 15:51:24 -0300

    fix: Remove web package

--- packages/web/src/entry.preview.tsx
+++
@@ -1,6 +0,0 @@
-import qwikCityPlan from '@qwik-city-plan'
-import { createQwikCity } from '@builder.io/qwik-city/middleware/node'
-
-import render from './entry.ssr'
-
-export default createQwikCity({ render, qwikCityPlan })
--- packages/web/src/utils/api.ts
+++
@@ -1,9 +0,0 @@
-// api
-import { getApiClient } from 'shared/api-client'
-
-// core
-export const apiClient = getApiClient({
-  isProduction: import.meta.env.NODE_ENV === 'production',
-  apiBaseUrlProduction: import.meta.env.PUBLIC_API_BASE_URL_PRODUCTION,
-  apiBaseUrlDevelopment: import.meta.env.PUBLIC_API_BASE_URL_DEVELOPMENT,
-})
