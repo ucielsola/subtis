c037599f15f5c4e5ef8fcfcd1dba9f5421d64396
lndgalante <lndgalante@gmail.com>
2023-12-30 11:15:45 -0300

    test: Add tests to check Elysia instance and routes definitions

---
+++ packages/api/app/app.test.ts
@@ -0,0 +1,52 @@
+import Elysia from 'elysia'
+import { describe, expect, it } from 'bun:test'
+
+// internals
+import { runApi } from './app'
+
+// constants
+const api = runApi(true, 8080)
+
+describe('API | runApi', () => {
+  it('returns a Elysia instance', () => {
+    expect(api).toBeInstanceOf(Elysia)
+  })
+
+  it('returns all defined routes', () => {
+    const apiRoutes = api.routes.map(({ path, method }) => ({ path, method }))
+    expect(apiRoutes).toEqual([
+      {
+        path: '/',
+        method: 'OPTIONS',
+      },
+      {
+        path: '/*',
+        method: 'OPTIONS',
+      },
+      {
+        path: '/v1/docs',
+        method: 'GET',
+      },
+      {
+        path: '/v1/docs/json',
+        method: 'GET',
+      },
+      {
+        path: '/v1/trending',
+        method: 'GET',
+      },
+      {
+        path: '/v1/movies',
+        method: 'POST',
+      },
+      {
+        path: '/v1/subtitle',
+        method: 'POST',
+      },
+      {
+        path: '/v1/subtitles',
+        method: 'POST',
+      },
+    ])
+  })
+})
