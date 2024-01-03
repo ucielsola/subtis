e24995a3fe88c908c4ff11b8f56628e9976c9f50
lndgalante <lndgalante@gmail.com>
2024-01-03 20:03:18 -0300

    feat: Add listener callback

--- packages/api/app/app.ts
+++ packages/api/app/app.ts
@@ -5,10 +5,10 @@ import { swagger } from '@elysiajs/swagger'
 import { rateLimit } from 'elysia-rate-limit'
 
 // internals
-import { getMoviesFromMovieId, getSubtitleFromFileName, getSubtitlesFromMovieId, getTrendingSubtitles } from '../index'
+import { getMoviesFromMovieId, getSubtitleFromFileName, getSubtitlesFromMovieId, getTrendingSubtitles, listener } from '../index'
 
 // core
-export function runApi(shouldDisplayInitialLog: boolean = false, port: number = 8080) {
+export function runApi(displayListenLog: boolean = false, port: number = 8080) {
   return new Elysia()
     .use(cors())
     .use(helmet())
@@ -18,13 +18,7 @@ export function runApi(shouldDisplayInitialLog: boolean = false, port: number = 
     .post('/v1/subtitle', getSubtitleFromFileName, { body: t.Object({ fileName: t.String() }) })
     .post('/v1/subtitles', getSubtitlesFromMovieId, { body: t.Object({ movieId: t.String() }) })
     .post('/v1/trending', getTrendingSubtitles, { body: t.Object({ limit: t.Number({ min: 1 }) }) })
-    .listen(port, (context) => {
-      if (shouldDisplayInitialLog) {
-        const { development, hostname, port } = context
-        // eslint-disable-next-line no-console
-        console.log(`\n🟢 Subtis API is running at http${development ? '' : 's'}://${hostname}:${port}\n`)
-      }
-    })
+    .listen(port, context => listener(context, displayListenLog))
 }
 
 // types
---
+++ packages/api/shared/listener.ts
@@ -0,0 +1,11 @@
+import type { Server } from 'bun'
+
+export function listener(context: Server, displayListenLog: boolean) {
+  if (!displayListenLog) {
+    return
+  }
+
+  const { development, hostname, port } = context
+  const message = `\n🟢 Subtis API is running at http${development ? '' : 's'}://${hostname}:${port}\n`
+  console.log(message) // eslint-disable-line no-console
+}
