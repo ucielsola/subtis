2ecbf1b8620bf8ac532023155ef0d285e9a9ffcb
Leonardo Galante <lndgalante@gmail.com>
2024-02-11 23:58:18 -0300

    feat: Remove old SubDivx methods

--- packages/indexer/tests/getSubDivXSearchPageHtml.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'bun:test'
-
-// internals
-import { getSubDivXSearchPageHtml } from '../subdivx'
-
-test('should return an search HTML for "Kinderfanger (2023)"', async () => {
-  const searchParams = await getSubDivXSearchPageHtml('Kinderfanger (2023)')
-  expect(searchParams).toBeTypeOf('string')
-})
