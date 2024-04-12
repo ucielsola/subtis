79976a329d10fd6b6d4e844064b5f9cc83dc6f90
lndgalante <lndgalante@gmail.com>
2024-04-12 18:08:58 -0300

    feat: Group file endpoints. Disable swagger due to bug.

--- packages/api/app/app.ts
+++ packages/api/app/app.ts
@@ -1,8 +1,8 @@
 import { cors } from "@elysiajs/cors";
-import { swagger } from "@elysiajs/swagger";
 import { Elysia, t } from "elysia";
 import { helmet } from "elysia-helmet";
 import { rateLimit } from "elysia-rate-limit";
+// import { swagger } from "@elysiajs/swagger";
 
 // internals
 import {
@@ -19,46 +19,51 @@ import {
 
 // core
 export function runApi(displayListenLog = false, port = 8080) {
-	return new Elysia()
-		.use(cors())
-		.use(helmet())
-		.use(swagger({ path: "/v1/docs" }))
-		.use(rateLimit({ skip: () => Bun.env.NODE_ENV !== "production" }))
-		.group("/v1/subtitles", (app) => {
-			return app
-				.post("/movie", getSubtitlesFromMovieId, {
-					body: t.Object({ movieId: t.String() }),
-				})
-				.post("/trending", getTrendingSubtitles, {
-					body: t.Object({ limit: t.Number({ min: 1 }) }),
-				})
-				.post("/file", getSubtitleFromFileName, {
-					body: t.Object({ bytes: t.String(), fileName: t.String() }),
-				})
-				.post("/file/versions", getSubtitleVersionsFromFileName, {
+	return (
+		new Elysia()
+			.use(cors())
+			.use(helmet())
+			.use(rateLimit({ skip: () => Bun.env.NODE_ENV !== "production" }))
+			// .use(swagger({ path: "/v1/docs" })) TODO: Fix Swagger -> https://github.com/elysiajs/elysia-swagger/issues/110
+			.group("/v1/subtitles", (app) => {
+				return app
+					.post("/movie", getSubtitlesFromMovieId, {
+						body: t.Object({ movieId: t.String() }),
+					})
+					.post("/trending", getTrendingSubtitles, {
+						body: t.Object({ limit: t.Number({ min: 1 }) }),
+					})
+					.group("/file", (app) => {
+						return app
+							.post("/", getSubtitleFromFileName, {
+								body: t.Object({ bytes: t.String(), fileName: t.String() }),
+							})
+							.post("/versions", getSubtitleVersionsFromFileName, {
+								body: t.Object({ fileName: t.String() }),
+							});
+					});
+			})
+			.group("/v1/movies", (app) => {
+				return app
+					.post("/title", getMoviesFromMovieTitle, {
+						body: t.Object({ movieTitle: t.String() }),
+					})
+					.post("/recent", getRecentMovies, {
+						body: t.Object({ limit: t.Number({ min: 1 }) }),
+					});
+			})
+			.group("/v1/integrations", (app) => {
+				return app.get("/stremio/:bytes/:fileName", getStremioSubtitleFromFileName, {
+					params: t.Object({ bytes: t.String(), fileName: t.String() }),
+				});
+			})
+			.group("/v1/metrics", (app) => {
+				return app.post("/download", getDownloadFromFileName, {
 					body: t.Object({ fileName: t.String() }),
 				});
-		})
-		.group("/v1/movies", (app) => {
-			return app
-				.post("/title", getMoviesFromMovieTitle, {
-					body: t.Object({ movieTitle: t.String() }),
-				})
-				.post("/recent", getRecentMovies, {
-					body: t.Object({ limit: t.Number({ min: 1 }) }),
-				});
-		})
-		.group("/v1/integrations", (app) => {
-			return app.get("/stremio/:bytes/:fileName", getStremioSubtitleFromFileName, {
-				params: t.Object({ bytes: t.String(), fileName: t.String() }),
-			});
-		})
-		.group("/v1/metrics", (app) => {
-			return app.post("/download", getDownloadFromFileName, {
-				body: t.Object({ fileName: t.String() }),
-			});
-		})
-		.listen(port, (context) => listener(context, displayListenLog));
+			})
+			.listen(port, (context) => listener(context, displayListenLog))
+	);
 }
 
 // types
