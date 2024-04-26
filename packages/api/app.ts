a8dfcc57557377444178a4741d2857e5ba8931d5
lndgalante <lndgalante@gmail.com>
2024-04-26 18:13:29 -0300

    feat: Format code

--- packages/api/app.ts
+++ packages/api/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { cache } from "hono/cache";
-import timestring from "timestring";
 import { secureHeaders } from "hono/secure-headers";
+import timestring from "timestring";
 
 // internals
 import { integrations } from "./integrations";
@@ -10,13 +10,12 @@ import { movies } from "./movies";
 import { shortener } from "./shortener";
 import { subtitles } from "./subtitles";
 
-
 // core
 const app = new Hono().basePath("/v1");
 
 // cache & middlewares
-app.use(secureHeaders())
-app.get('*', cache({ cacheName: 'subtis-api', cacheControl: `s-maxage=${timestring("1 week")}` }));
+app.use(secureHeaders());
+app.get("*", cache({ cacheName: "subtis-api", cacheControl: `s-maxage=${timestring("1 week")}` }));
 
 // routes
 const routes = app
