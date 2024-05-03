a174e1daa83a8ef37f0c4fd51037bb44633d9db6
lndgalante <lndgalante@gmail.com>
2024-05-03 19:36:40 -0300

    feat: Remove tiny-invariant. Add zod library.

--- packages/raycast/src/invariant.ts
+++
@@ -1,8 +0,0 @@
-export function getIsInvariantError(error: Error): boolean {
-	const INVARIANT_ERROR = "Invariant" as const;
-	return error.message.includes(INVARIANT_ERROR);
-}
-
-export function getParsedInvariantMessage(error: Error): string {
-	return error.message.slice(18);
-}
