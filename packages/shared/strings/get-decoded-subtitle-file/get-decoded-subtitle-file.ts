65fb30cdb098210b830cc6a2e6890090446d5aa7
lndgalante <lndgalante@gmail.com>
2024-09-02 22:50:53 -0300

    feat: Avoid windows-1251 as a encoding parameter

--- packages/shared/strings/get-decoded-subtitle-file/get-decoded-subtitle-file.ts
+++ packages/shared/strings/get-decoded-subtitle-file/get-decoded-subtitle-file.ts
@@ -1,4 +1,11 @@
 export function getDecodedSubtitleFile(subtitleBuffer: Buffer, encoding?: string): string {
-  const decoder = new TextDecoder(encoding ?? "iso-8859-1");
+  let parsedEncoder = "iso-8859-1";
+
+  if (encoding && encoding !== "windows-1251") {
+    parsedEncoder = encoding;
+  }
+
+  const decoder = new TextDecoder(parsedEncoder);
+
   return decoder.decode(subtitleBuffer);
 }
