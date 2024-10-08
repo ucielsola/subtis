f8914160a806c14d62322098e0e1cbb37b7575ab
Leonardo Galante <lndgalante@gmail.com>
2024-10-08 00:23:23 -0300

    feat: Improve cinema regex filtering

--- packages/indexer/app.ts
+++ packages/indexer/app.ts
@@ -684,7 +684,7 @@ function getFilteredTorrents(
   maxTorrents = 25,
 ): TorrentFoundWithId[] {
   const CINEMA_RECORDING_REGEX =
-    /\b(hdcam|hdcamrip|hqcam|hq-cam|telesync|hdts|hd-ts|c1nem4|qrips|hdrip|cam|soundtrack|xxx|clean|khz|ep|camrip)\b/gi;
+    /\b(hdcam|hdcamrip|hqcam|hq-cam|telesync|hdts|hd-ts|c1nem4|qrips|hdrip|cam|soundtrack|xxx|clean|khz|ep|camrip|dvdscr)\b/gi;
 
   const seenTitles = new Set<string>();
   const seenSizes = new Set<string | number>();
