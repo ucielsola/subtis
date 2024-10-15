9f1a23ea34559727fe4d48181b57dc00e055f3e0
Leonardo Galante <lndgalante@gmail.com>
2024-10-15 01:30:04 -0300

    feat: Use production api only

--- packages/api/shared/links.ts
+++ packages/api/shared/links.ts
@@ -1,5 +1,3 @@
 export function getSubtitleShortLink(id: number): string {
-  return process.env.NODE_ENV === "production"
-    ? `https://api.subt.is/v1/subtitle/link/${id}`
-    : `http://localhost:58602/v1/subtitle/link/${id}`;
+  return `https://api.subt.is/v1/subtitle/link/${id}`;
 }
