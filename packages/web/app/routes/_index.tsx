6cc2c9e497f47ae247de6ebc3adf8bfd46489103
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-10-17 12:48:28 -0300

    feat: initial remix + cloudflare setup + list movies

--- packages/web/app/routes/_index.tsx
+++ packages/web/app/routes/_index.tsx
@@ -1,20 +1,49 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { AppLoadContext, LoaderFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import {getApiClient} from "~/utils/api-client.server"
+import { getEnv } from "~/utils/env.server";
 
-export function loader() {
-  console.log('running loader')
-  return null
+export async function loader({context}: LoaderFunctionArgs) {
+  const movies = await fetchMovies(context)
+  if ("message" in movies) {
+    throw new Error(`there was an error while fetch movies: ${movies.message}`)
+  }
+  return {movies}
 }
 
 export const meta: MetaFunction = () => {
   return [
     { title: "Subtis" },
-    { name: "description", content: "Welcome to Subtis!" },
+    { name: "description", content: "Welcome to Subtis" },
   ];
 };
 
 export default function Index() {
+  const {movies} = useLoaderData<typeof loader>()
   return (
-    <span>home</span>
+    <>
+    <h1>home page</h1>
+    <ul>
+        {movies.map((movie) => {
+          return (
+            <li key={`movie-${movie.id}`}>
+              {movie.title_name}
+              {movie.id}
+            </li>
+          )
+          })}
+      </ul>
+</>
   );
 }
 
+async function fetchMovies(context: AppLoadContext) {
+  const env = getEnv(context)
+  const apiClient = getApiClient({apiBaseUrl: env.PUBLIC_API_BASE_URL_PRODUCTION})
+  const response = await apiClient.v1.titles.trending[":limit"].$get({
+    param: {
+    limit: "10"
+    }
+  })
+  return response.json()
+}
---
+++ packages/web/app/utils/env.server.ts
@@ -0,0 +1,17 @@
+import type { AppLoadContext } from "@remix-run/cloudflare"
+
+export interface Env {
+  SUPABASE_API_KEY: string,
+  SUPABASE_BASE_URL: string,
+  OPEN_SUBTITLES_API_KEY: string,
+  TMDB_API_KEY: string,
+  YOUTUBE_API_KEY: string,
+  PUBLIC_API_BASE_URL_DEVELOPMENT: string,
+  PUBLIC_API_BASE_URL_PRODUCTION: string
+}
+
+export function getEnv(context: AppLoadContext): Env {
+  console.log("context.cloudflare.env", context.cloudflare.env)
+  return context.cloudflare.env
+}
+
