3259997c8ed994f78a080dd347375461a2d3c470
lndgalante <lndgalante@gmail.com>
2024-10-27 04:50:21 -0300

    feat: Bump project version

--- packages/indexer/opensubtitles.ts
+++ packages/indexer/opensubtitles.ts
@@ -26,7 +26,7 @@ function getOpenSubtitlesHeaders(): {
   "Content-Type": "application/json";
 } {
   return {
-    "User-Agent": "Subtis v0.3.5",
+    "User-Agent": "Subtis v0.3.6",
     "Content-Type": "application/json",
     "Api-Key": getOpenSubtitlesApiKey(),
   };
