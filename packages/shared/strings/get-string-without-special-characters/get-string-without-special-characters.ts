0d43e4fa65cb2c2896baaa2d98bb17effc63187e
lndgalante <lndgalante@gmail.com>
2024-11-08 20:17:39 -0300

    fix: Issue for comma in title names

--- packages/shared/strings/get-string-without-special-characters/get-string-without-special-characters.ts
+++ packages/shared/strings/get-string-without-special-characters/get-string-without-special-characters.ts
@@ -3,6 +3,7 @@ import replaceSpecialCharacters from "replace-special-characters";
 export function getStringWithoutSpecialCharacters(str: string): string {
   return replaceSpecialCharacters(str.toLowerCase())
     .replaceAll(":", "")
+    .replaceAll(",", "")
     .replaceAll(" - ", " ")
     .replaceAll(".", "")
     .replaceAll("'", "");
