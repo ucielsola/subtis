4693ed3ba4df8432269d801ebf167e4095207dae
lndgalante <lndgalante@gmail.com>
2024-11-22 03:11:05 -0300

    feat: Improve trending wording

--- packages/web/app/components/home/trending.tsx
+++ packages/web/app/components/home/trending.tsx
@@ -7,16 +7,16 @@ export function HomeTrending() {
     <section className="py-16 flex flex-col gap-32">
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-2">
-          <h3 className="text-slate-950 text-3xl font-semibold">Trending</h3>
-          <h4 className="text-slate-600">Descubrí los títulos más descargados del momento.</h4>
+          <h3 className="text-slate-950 text-3xl font-semibold">Subtítulos más buscados</h3>
+          <h4 className="text-slate-600">Descubrí los subtítulos más descargados del momento.</h4>
         </div>
         <TrendingSlider />
       </div>
 
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-2">
-          <h3 className="text-slate-950 text-3xl font-semibold">Novedades</h3>
-          <h4 className="text-slate-600">Encontrá los últimos títulos agregados a la plataforma.</h4>
+          <h3 className="text-slate-950 text-3xl font-semibold">Últimos subtítulos cargados</h3>
+          <h4 className="text-slate-600">Descarga los subtítulos para las últimas películas agregadas.</h4>
         </div>
         <NewsSlider />
       </div>
