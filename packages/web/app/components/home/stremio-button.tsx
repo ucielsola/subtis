16a913137a5b54070d8d43e50b4c3e8d08b44b75
lndgalante <lndgalante@gmail.com>
2024-11-22 03:13:20 -0300

    feat: Improve stremio and VLC border thickness

--- packages/web/app/components/home/stremio-button.tsx
+++ packages/web/app/components/home/stremio-button.tsx
@@ -139,7 +139,7 @@ export function StremioButton() {
       </Sheet>
 
       <Button
-        className="bg-slate-950 group border-[#1155D9] border relative overflow-hidden"
+        className="bg-slate-950 group border-[#1155D9] border-2 relative overflow-hidden"
         onClick={handleToggleIsOpen}
       >
         <div className="absolute inset-0 bg-gradient-to-br from-[#1155D9] to-[#7B5BF5] transition-all ease-in-out -translate-y-full group-hover:translate-y-0" />
--- packages/web/app/components/home/vlc-button.tsx
+++ packages/web/app/components/home/vlc-button.tsx
@@ -302,7 +302,7 @@ export function VlcButton() {
       </Sheet>
 
       <Button
-        className="bg-slate-950 group border-[#E26400] border relative overflow-hidden"
+        className="bg-slate-950 group border-[#E26400] border border-2 relative overflow-hidden"
         onClick={handleToggleIsOpen}
       >
         <div className="absolute inset-0 bg-gradient-to-br from-[#E26400] to-[#FF9800] transition-all ease-in-out -translate-y-full group-hover:translate-y-0 w-24" />
