1d3f40ce9299a52ff49e12e9100f960623704412
lndgalante <lndgalante@gmail.com>
2024-12-01 03:57:13 -0300

    feat: Add MGB release group

--- packages/indexer/release-groups.ts
+++ packages/indexer/release-groups.ts
@@ -507,6 +507,11 @@ export const RELEASE_GROUPS = {
       "h265-freecrystaltarantulaofsunshine",
     ],
   },
+  MgB: {
+    is_supported: true,
+    release_group_name: "MgB",
+    matches: ["MgB", "H.265-MgB", "265-MgB", "H265-MgB", "H.264-MgB", "264-MgB", "H264-MgB"],
+  },
 } as const;
 
 // types
