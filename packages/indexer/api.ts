2be69320181af889bdc13aed51e410d222fbd2b2
lndgalante <lndgalante@gmail.com>
2024-12-13 19:44:35 -0300

    feat: Fix point to real prod api

--- packages/indexer/api.ts
+++ packages/indexer/api.ts
@@ -4,5 +4,5 @@ import { getApiClient } from "@subtis/shared";
 const isProduction = process.env.NODE_ENV === "production";
 
 export const apiClient = getApiClient({
-  apiBaseUrl: !isProduction ? process.env.PUBLIC_API_BASE_URL_PRODUCTION : process.env.PUBLIC_API_BASE_URL_DEVELOPMENT,
+  apiBaseUrl: isProduction ? process.env.PUBLIC_API_BASE_URL_PRODUCTION : process.env.PUBLIC_API_BASE_URL_DEVELOPMENT,
 });
