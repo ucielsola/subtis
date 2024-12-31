bd060141dc63ff8b4ee202a455f7af17375bce9c
lndgalante <lndgalante@gmail.com>
2024-12-31 13:50:58 -0300

    feat: Handle edge case for Justice movie

--- packages/indexer/edge-cases.ts
+++ packages/indexer/edge-cases.ts
@@ -1,4 +1,5 @@
 export const FILE_NAME_TO_TMDB_INDEX = {
   "X-Men.2000.1080p.BrRip.x264.YIFY.mp4": 1,
+  "Justice.2024.720p.WEBRip.x264.AAC-[YTS.MX].mp4": 1,
   "The.Beast.2023.1080p.BluRay.x264.AAC5.1-WORLD.mp4": 2,
 };
