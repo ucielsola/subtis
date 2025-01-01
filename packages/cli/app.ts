79308de63e42655cdd20091cd47e86078baec358
lndgalante <lndgalante@gmail.com>
2024-12-31 23:23:58 -0300

    feat: Bump to v0.4.8

--- packages/cli/app.ts
+++ packages/cli/app.ts
@@ -275,7 +275,7 @@ const program = new Command();
 program
   .name("subtis")
   .description("CLI to search for movie subtitles")
-  .version("0.4.7")
+  .version("0.4.8")
   .command("search")
   .description("Search a subtitle for a video file")
   .argument("<file>", "Video file")
--- packages/stremio/app.ts
+++ packages/stremio/app.ts
@@ -61,7 +61,7 @@ async function getTitleSubtitle(args: Args): Promise<{ subtitles: StremioSubtitl
 const builder = new addonBuilder({
   name: "Subtis (Version Pre-Beta)",
   id: "org.subtis",
-  version: "0.4.7",
+  version: "0.4.8",
   description:
     "Subtis es tu fuente de subtitulos para tus películas y series favoritas. Esta es una versión de prueba interna, solo para desarrolladores.",
   catalogs: [],
