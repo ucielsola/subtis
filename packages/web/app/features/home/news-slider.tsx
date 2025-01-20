099e8fcb6b15a346c3e797022d1343effe07aeee
lndgalante <lndgalante@gmail.com>
2025-01-20 16:11:56 -0300

    feat: Implement new carousel in News slider

--- packages/web/app/features/home/news-slider.tsx
+++ packages/web/app/features/home/news-slider.tsx
@@ -6,6 +6,9 @@ import type { loader } from "~/routes/_index";
 // internals
 import { ThumbHashNewsImage } from "./thumbhash-news-image";
 
+// ui
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "~/components/ui/carousel";
+
 export function NewsSlider() {
   const { recentDownloadedTitles } = useLoaderData<typeof loader>();
 
@@ -14,25 +17,31 @@ export function NewsSlider() {
   }
 
   return (
-    <div className="inline-flex overflow-x-scroll [scroll-snap-type:x_mandatory] [scroll-behavior:smooth] [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden rounded-md gap-3 py-3">
-      {recentDownloadedTitles.results.map((title) => {
-        if (!title.backdrop) {
-          return null;
-        }
-        if (title.type !== "movie") {
-          return null;
-        }
+    <Carousel className="w-full">
+      <CarouselContent className="p-4">
+        {recentDownloadedTitles.results.map((title) => {
+          if (!title.backdrop) {
+            return null;
+          }
+          if (title.type !== "movie") {
+            return null;
+          }
 
-        return (
-          <Link
-            key={title.id}
-            to={`/subtitles/movie/${title.imdb_id}`}
-            className="box-content flex flex-none [scroll-snap-align:start] rounded-sm overflow-hidden cursor-pointer hover:scale-105 transition-all ease-in-out duration-300 group will-change-transform"
-          >
-            <ThumbHashNewsImage src={title.backdrop} hashUrl={title.backdrop_thumbhash} alt={title.title_name} />
-          </Link>
-        );
-      })}
-    </div>
+          return (
+            <CarouselItem key={title.id} className="basis-auto pl-3">
+              <Link
+                key={title.id}
+                to={`/subtitles/movie/${title.imdb_id}`}
+                className="box-content flex flex-none rounded-sm overflow-hidden cursor-pointer hover:scale-105 transition-all ease-in-out duration-300 group will-change-transform"
+              >
+                <ThumbHashNewsImage src={title.backdrop} hashUrl={title.backdrop_thumbhash} alt={title.title_name} />
+              </Link>
+            </CarouselItem>
+          );
+        })}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 }
