59a27d68ec57b212269484f89d0cb8844bc6c138
lndgalante <lndgalante@gmail.com>
2025-01-24 20:39:20 -0300

    feat: Add Batman The Dark Knight movie edge case

--- packages/indexer/edge-cases.ts
+++ packages/indexer/edge-cases.ts
@@ -2,4 +2,5 @@ export const FILE_NAME_TO_TMDB_INDEX = {
   "X-Men.2000.1080p.BrRip.x264.YIFY.mp4": 1,
   "Justice.2024.720p.WEBRip.x264.AAC-[YTS.MX].mp4": 1,
   "The.Beast.2023.1080p.BluRay.x264.AAC5.1-WORLD.mp4": 2,
+  "Batman.The.Dark.Knight.2008.1080p.BluRay.x264.YIFY.mp4": 1,
 };
