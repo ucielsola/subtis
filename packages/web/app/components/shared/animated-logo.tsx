c17363bee3fd329cf02b4693ecf2e0e61ec09216
lndgalante <lndgalante@gmail.com>
2025-01-28 19:29:30 -0300

    feat: Improve animated logo to always be active

--- packages/web/app/components/shared/animated-logo.tsx
+++ packages/web/app/components/shared/animated-logo.tsx
@@ -4,10 +4,10 @@ import { Logo } from "~/components/layout/logo";
 export function AnimatedLogo() {
   return (
     <div className="relative w-fit h-fit isolate">
-      <div className="border-2 relative z-10 border-[#0E0C14] w-[76px] h-[29px] flex items-center justify-center rounded-[6px] bg-white text-[#0E0C14] transition-all duration-300 ease-in-out group-hover:-translate-x-[3px] group-hover:-translate-y-[3px] group-active:-translate-x-[1.5px] group-active:-translate-y-[1.5px]">
+      <div className="border-2 relative z-10 border-[#0E0C14] w-[76px] h-[29px] flex items-center justify-center rounded-[6px] bg-zinc-50 text-[#0E0C14] transition-all duration-300 ease-in-out -translate-x-[3px] -translate-y-[3px] group-active:-translate-x-[1px] group-active:-translate-y-[1px] group-hover:-translate-x-[1.5px] group-hover:-translate-y-[1.5px]">
         <Logo />
       </div>
-      <div className="absolute inset-0 rounded-[6px] bg-white" />
+      <div className="absolute inset-0 rounded-[6px] bg-zinc-50" />
     </div>
   );
 }
