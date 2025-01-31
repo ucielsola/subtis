8c526500d8d928b08d992461919a401a1361146f
lndgalante <lndgalante@gmail.com>
2025-01-31 01:31:01 -0300

    feat: Add modal mode to Stremio

--- packages/web/app/features/home/stremio-button.tsx
+++ packages/web/app/features/home/stremio-button.tsx
@@ -33,7 +33,7 @@ export function StremioButton() {
 
   return (
     <Fragment>
-      <Sheet open={isOpen} onOpenChange={handleToggleIsOpen} modal={false}>
+      <Sheet open={isOpen} onOpenChange={handleToggleIsOpen}>
         <SheetContent className="overflow-y-auto bg-zinc-950 border-zinc-700 border rounded-sm my-3 mr-3 max-h-[calc(100vh-24px)] sm:max-w-md">
           <SheetHeader className="mb-10">
             <SheetTitle className="text-zinc-50">Instalación de Subtis para Stremio</SheetTitle>
