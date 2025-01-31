63b540606c8fbf6d280ffe12afa371e79845dc09
lndgalante <lndgalante@gmail.com>
2025-01-31 12:20:47 -0300

    feat: Update cronjob logic

--- packages/indexer/cron.ts
+++ packages/indexer/cron.ts
@@ -8,7 +8,7 @@ import cron from "node-cron";
 // internals
 import { optimizeTitleTableImages } from "./image-optimizer";
 import { indexMoviesByYear } from "./movies";
-import { indexNotFoundSubtitles } from "./not-found";
+// import { indexNotFoundSubtitles } from "./not-found";
 // import { saveSubtitleGroupsToDb } from "./subtitle-groups";
 // import { saveReleaseGroupsToDb } from "./release-groups";
 // import { indexSeriesByYear } from "./tv-shows";
@@ -22,22 +22,21 @@ cron.schedule("30 19 * * *", async () => {
   const dayOfYearValue = today.dayOfYear();
   console.log("\n ~ cron.schedule ~ dayOfYearValue:", dayOfYearValue);
 
-  // if (dayOfYearValue % 2 === 0) {
   console.log("Indexing movies...");
   await indexMoviesByYear({
     year: 2024,
     indexFromPage: 52,
     isDebugging: false,
   });
+
+  console.log("Optimizing title table images...");
   await optimizeTitleTableImages();
+
+  // if (dayOfYearValue % 2 === 0) {
   // } else {
   //   console.log("Indexing tv shows...");
   //   await indexSeriesByYear(2024, false);
   // }
-
-  console.log("Indexing not found subtiles...");
-  await indexNotFoundSubtitles();
-  await optimizeTitleTableImages();
 });
 
 // update release and subtitle groups
