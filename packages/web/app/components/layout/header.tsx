8d14ef8a44fb373b2b6f7e24792592bfe9b51141
lndgalante <lndgalante@gmail.com>
2025-02-01 08:45:27 -0300

    feat: New header to avoid focus on search by movie

--- packages/web/app/components/layout/header.tsx
+++ packages/web/app/components/layout/header.tsx
@@ -3,17 +3,18 @@ import { Link } from "@remix-run/react";
 // components
 import { AnimatedLogo } from "~/components/shared/animated-logo";
 
-// internals
-import { SearchButton } from "./search-button";
-
 export function Header() {
   return (
-    <header className="absolute z-40 top-0 left-0 right-0 container mx-auto">
-      <nav className="flex items-center justify-between py-4 px-4 w-full">
+    <header className="absolute z-40 top-4 left-0 right-0 container mx-auto">
+      <nav className="flex items-center justify-between px-5 py-3.5 w-full bg-zinc-950/60 backdrop-blur-md rounded-full">
         <Link to="/" className="cursor-pointer group">
           <AnimatedLogo />
         </Link>
-        <SearchButton />
+        <div className="flex items-center gap-2">
+          <Link to="/search" className="text-sm text-zinc-50 font-medium hover:underline">
+            Buscar por película
+          </Link>
+        </div>
       </nav>
     </header>
   );
