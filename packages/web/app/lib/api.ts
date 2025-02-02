6115e24db262628df2da6a06b38ac20b7deaf759
lndgalante <lndgalante@gmail.com>
2025-02-02 01:09:39 -0300

    feat: Rollback apiBaseUrl

--- packages/web/app/lib/api.ts
+++ packages/web/app/lib/api.ts
@@ -2,6 +2,6 @@
 import { getApiClient } from "@subtis/shared";
 
 export const apiClient = getApiClient({
-  // apiBaseUrl: "https://api.subt.is",
-  apiBaseUrl: "http://localhost:58602",
+  apiBaseUrl: "https://api.subt.is",
+  // apiBaseUrl: "http://localhost:58602",
 });
