1411180641147cdbaef5912c6a2fc8045ed280a4
lndgalante <lndgalante@gmail.com>
2025-03-14 09:15:27 -0300

    feat: Bump project dependencies. Update indexed from page 97

--- packages/indexer/movies-year.ts
+++ packages/indexer/movies-year.ts
@@ -17,7 +17,7 @@ import { indexMoviesByYear } from "./movies";
 indexMoviesByYear({
   year: 2025,
   isDebugging: false,
-  indexFromPage: 0,
+  indexFromPage: 97,
 });
 
 // saveReleaseGroupsToDb(supabase);
