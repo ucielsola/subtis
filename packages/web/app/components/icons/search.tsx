7136a6529834ac519b2ad3aad67fa2c80267a018
lndgalante <lndgalante@gmail.com>
2025-03-16 01:54:37 -0300

    feat: Use icon instead of label for search link

--- packages/web/app/components/icons/search.tsx
+++ packages/web/app/components/icons/search.tsx
@@ -1,35 +1,34 @@
 import { type AnimationControls, motion } from "motion/react";
 
-export function SearchIcon({ controls }: { controls: AnimationControls }) {
+export function SearchIcon({ controls, className }: { controls: AnimationControls; className?: string }) {
   return (
-    <div className="select-none p-[2px]">
-      <motion.svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="28"
-        height="28"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        variants={{
-          normal: { x: 0, y: 0 },
-          animate: {
-            x: [0, 0, -3, 0],
-            y: [0, -3, 0, 0],
-          },
-        }}
-        transition={{
-          duration: 1,
-          bounce: 0.3,
-        }}
-        animate={controls}
-      >
-        <title>Lupa</title>
-        <circle cx="11" cy="11" r="8" />
-        <path d="m21 21-4.3-4.3" />
-      </motion.svg>
-    </div>
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="28"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      variants={{
+        normal: { x: 0, y: 0 },
+        animate: {
+          x: [0, 0, -3, 0],
+          y: [0, -3, 0, 0],
+        },
+      }}
+      transition={{
+        duration: 1,
+        bounce: 0.3,
+      }}
+      animate={controls}
+      className={className}
+    >
+      <title>Lupa</title>
+      <circle cx="11" cy="11" r="8" />
+      <path d="m21 21-4.3-4.3" />
+    </motion.svg>
   );
 }
--- packages/web/app/components/layout/header.tsx
+++ packages/web/app/components/layout/header.tsx
@@ -1,9 +1,14 @@
 import { Link } from "@remix-run/react";
+import { useAnimation } from "motion/react";
 
 // components
+import { SearchIcon } from "~/components/icons/search";
 import { AnimatedLogo } from "~/components/shared/animated-logo";
 
 export function Header() {
+  // motion hooks
+  const controls = useAnimation();
+
   return (
     <header className="absolute z-40 top-6 left-0 right-0 container mx-auto px-4">
       <nav className="flex items-center justify-between w-full">
@@ -12,9 +17,11 @@ export function Header() {
         </Link>
         <Link
           to="/search"
-          className="text-zinc-50 text-sm font-semibold px-4 py-1.5 bg-zinc-50/20 backdrop-blur-lg rounded-full hover:bg-zinc-950/80 transition-all ease-in-out duration-300"
+          className="text-zinc-50 text-sm font-semibold p-2 bg-zinc-50/20 backdrop-blur rounded-full hover:bg-zinc-50/25 transition-all ease-in-out duration-300"
+          onMouseEnter={() => controls.start("animate")}
+          onMouseLeave={() => controls.start("normal")}
         >
-          Buscar por nombre
+          <SearchIcon controls={controls} className="size-5" />
         </Link>
       </nav>
     </header>
