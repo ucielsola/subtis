fc04a2753381f7b7c6d3454eee2e486c837a35ed
lndgalante <lndgalante@gmail.com>
2025-03-19 22:02:28 -0300

    feat: Broken edge case for X-Men

--- packages/indexer/edge-cases.ts
+++ packages/indexer/edge-cases.ts
@@ -1,5 +1,5 @@
 export const FILE_NAME_TO_TMDB_INDEX = {
-  "X-Men.2000.1080p.BrRip.x264.YIFY.mp4": 1,
+  "X-Men.2000.1080p.BrRip.x264.YIFY.mp4": 0,
   "Justice.2024.720p.WEBRip.x264.AAC-[YTS.MX].mp4": 1,
   "The.Beast.2023.1080p.BluRay.x264.AAC5.1-WORLD.mp4": 2,
   "Batman.The.Dark.Knight.2008.1080p.BluRay.x264.YIFY.mp4": 1,
