9c93d49c187db517d8e70d92f834b7c0d372fd98
lndgalante <lndgalante@gmail.com>
2025-04-11 22:21:05 -0300

    feat: Avoid hidden on mobile devices

--- packages/web/app/components/shared/video-dropzone.tsx
+++ packages/web/app/components/shared/video-dropzone.tsx
@@ -97,7 +97,7 @@ export function VideoDropzone() {
                   Seleccionar archivo
                 </Button>
                 <div className="flex flex-col gap-0 z-10 bg-zinc-950 rounded-sm mt-6">
-                  <p className="text-sm text-zinc-300 text-center hidden lg:block w-fit container mx-auto">
+                  <p className="text-sm text-zinc-300 text-center w-fit container mx-auto">
                     Si tenés el archivo de tu película, arrastralo acá
                   </p>
                   <p className="text-[11px] text-zinc-400 text-center w-fit container mx-auto">
