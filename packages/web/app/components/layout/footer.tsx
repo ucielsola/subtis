0ff3818fa0c11abb3b17c3dd14b1739b0fdaacba
lndgalante <lndgalante@gmail.com>
2025-04-11 21:38:30 -0300

    feat: Update new footer UI

--- packages/web/app/components/layout/footer.tsx
+++ packages/web/app/components/layout/footer.tsx
@@ -2,7 +2,11 @@ import { Link } from "react-router";
 import { toast } from "sonner";
 import { useCopyToClipboard } from "usehooks-ts";
 
-// components
+import { GitHub } from "~/components/logos/github";
+// logos
+import { X } from "~/components/logos/x";
+
+// shared components
 import { AnimatedLogo } from "~/components/shared/animated-logo";
 
 export function HomeFooter() {
@@ -18,77 +22,75 @@ export function HomeFooter() {
   }
 
   return (
-    <footer className="pt-8 pb-16 flex flex-col md:flex-row justify-between items-start gap-6 md:gap-4 border-t border-zinc-700">
-      <div className="flex flex-col gap-2 items-start">
+    <footer className="pt-8 pb-20 flex flex-col gap-6 border-t border-zinc-700">
+      <div className="flex md:flex-row flex-col gap-6 justify-between items-start">
         <Link to="/" className="cursor-pointer group">
           <AnimatedLogo />
         </Link>
-        <span className="text-[13px] text-zinc-50 text-left">Subtis &#169; 2025</span>
+
+        <div className="flex flex-row gap-16">
+          <div className="flex flex-col gap-2 py-2">
+            <span className="text-xs font-bold text-zinc-400 mb-1">Soporte</span>
+            <a
+              href="https://subtis.canny.io/feature-requests"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xs font-medium text-zinc-50 hover:underline"
+            >
+              Pedir una feature
+            </a>
+            <button
+              type="button"
+              onClick={handleCopyEmailToClipboard}
+              className="text-xs font-medium text-zinc-50 hover:underline inline-flex"
+            >
+              Contacto vía email
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-2 py-2">
+            <span className="text-xs font-bold text-zinc-400 mb-1">Desarrolladores</span>
+            <a
+              href="https://api.subt.is/v1/docs"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xs font-medium text-zinc-50 hover:underline"
+            >
+              Conectate a la API
+            </a>
+            <a
+              href="https://github.com/lndgalante/subtis"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xs font-medium text-zinc-50 hover:underline"
+            >
+              Repositorio en GitHub
+            </a>
+          </div>
+
+          <div className="flex flex-col gap-2 py-2">
+            <span className="text-xs font-bold text-zinc-400 mb-1">Legal</span>
+            <Link to="/faq" className="text-xs font-medium text-zinc-50 hover:underline">
+              Preguntas Frecuentes
+            </Link>
+            <Link to="/terms" className="text-xs font-medium text-zinc-50 hover:underline">
+              Términos y Condiciones
+            </Link>
+          </div>
+        </div>
       </div>
 
-      <div className="flex flex-col gap-2 py-2">
-        <span className="text-xs font-bold text-zinc-300 mb-1">Desarrolladores</span>
-        <a
-          href="https://api.subt.is/v1/docs"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-xs font-medium text-zinc-50  hover:underline"
-        >
-          Conectate a la API
-        </a>
-        <a
-          href="https://github.com/lndgalante/subtis"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-xs font-medium text-zinc-50  hover:underline"
-        >
-          Repositorio en GitHub
-        </a>
-      </div>
+      <div className="border-t border-zinc-900/60 flex flex-row justify-between items-center flex-1 pt-7 mt-20">
+        <span className="text-xs text-zinc-400 text-left">&#169; Subtis {new Date().getFullYear()}</span>
 
-      <div className="flex flex-col gap-2 py-2">
-        <span className="text-xs font-bold text-zinc-300 mb-1">Soporte</span>
-        <a
-          href="https://subtis.canny.io/feature-requests"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-xs font-medium text-zinc-50  hover:underline"
-        >
-          Pedir una feature
-        </a>
-        <button
-          type="button"
-          onClick={handleCopyEmailToClipboard}
-          className="text-xs font-medium text-zinc-50  hover:underline inline-flex"
-        >
-          Contacto vía email
-        </button>
-      </div>
-
-      <div className="flex flex-col gap-2  py-2">
-        <span className="text-xs font-bold text-zinc-300 mb-1">Legal</span>
-        <Link to="/faq" className="text-xs font-medium text-zinc-50  hover:underline">
-          Preguntas Frecuentes
-        </Link>
-        <Link to="/terms" className="text-xs font-medium text-zinc-50  hover:underline">
-          Términos y Condiciones
-        </Link>
-      </div>
-
-      <div className="flex flex-col gap-2 py-2">
-        <span className="text-xs font-bold text-zinc-300 mb-1">Redes</span>
-        <a
-          href="https://x.com/subt_is"
-          target="_blank"
-          className="text-xs font-medium text-zinc-50 hover:underline flex items-center gap-1.5"
-          rel="noopener noreferrer"
-        >
-          Seguinos en
-          <img src="/x.svg" alt="X" className="w-4 h-4" />
-        </a>
-        <span className="text-xs font-medium text-zinc-50 hover:underline flex items-center gap-1.5 cursor-wait">
-          Pronto en Instagram
-        </span>
+        <div className="flex flex-row gap-4">
+          <a href="https://x.com/subt_is" target="_blank" rel="noopener noreferrer">
+            <X className="w-4 h-4 fill-zinc-400 hover:fill-zinc-50 transition-all duration-300 ease-in-out" />
+          </a>
+          <a href="https://github.com/lndgalante/subtis" target="_blank" rel="noopener noreferrer">
+            <GitHub className="w-4 h-4 fill-zinc-400 hover:fill-zinc-50 transition-all duration-300 ease-in-out" />
+          </a>
+        </div>
       </div>
     </footer>
   );
