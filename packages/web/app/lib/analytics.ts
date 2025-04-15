062a0890a940dd41b80b1995baf2935275797bac
lndgalante <lndgalante@gmail.com>
2025-04-15 20:31:58 -0300

    feat: Move OpenPanel s to Cloudflare

--- packages/web/app/lib/analytics.ts
+++ packages/web/app/lib/analytics.ts
@@ -1,9 +1,19 @@
 import { OpenPanel } from "@openpanel/web";
+import { z } from "zod";
+
+// helpers
+function getOpenPanelClientId() {
+  return z.string().parse(process.env.OPEN_PANEL_CLIENT_ID);
+}
+
+function getOpenPanelClientSecret() {
+  return z.string().parse(process.env.OPEN_PANEL_CLIENT_SECRET);
+}
 
 export const op = new OpenPanel({
   trackAttributes: true,
   trackScreenViews: true,
   trackOutgoingLinks: true,
-  clientSecret: "sec_c14a9284f6296ec34622",
-  clientId: "1bd46920-0e33-431e-b68f-dc6dd97be9dc",
+  clientId: getOpenPanelClientId(),
+  clientSecret: getOpenPanelClientSecret(),
 });
