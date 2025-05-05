d91537828c5b1048183ac09a4cab0c00ddcbb1aa
lndgalante <lndgalante@gmail.com>
2025-05-05 20:54:45 -0300

    feat: Add Copy animated icon

---
+++ packages/web/app/components/icons/copy.tsx
@@ -0,0 +1,61 @@
+import { type Transition, motion, type useAnimation } from "motion/react";
+import type { HTMLAttributes } from "react";
+
+export interface CopyIconHandle {
+  startAnimation: () => void;
+  stopAnimation: () => void;
+}
+
+interface CopyIconProps extends HTMLAttributes<HTMLDivElement> {
+  size?: number;
+  controls?: ReturnType<typeof useAnimation>;
+}
+
+const defaultTransition: Transition = {
+  type: "spring",
+  stiffness: 160,
+  damping: 17,
+  mass: 1,
+};
+
+export function CopyIcon({ controls, className, size = 28 }: CopyIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <title>Copiar</title>
+      <motion.rect
+        width="14"
+        height="14"
+        x="8"
+        y="8"
+        rx="2"
+        ry="2"
+        variants={{
+          normal: { translateY: 0, translateX: 0 },
+          animate: { translateY: -3, translateX: -3 },
+        }}
+        animate={controls}
+        transition={defaultTransition}
+      />
+      <motion.path
+        d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"
+        variants={{
+          normal: { x: 0, y: 0 },
+          animate: { x: 3, y: 3 },
+        }}
+        transition={defaultTransition}
+        animate={controls}
+      />
+    </svg>
+  );
+}
