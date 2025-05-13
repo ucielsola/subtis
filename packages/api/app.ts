e98a8227fad07a6f76e8698672bbd393eb85498f
lndgalante <lndgalante@gmail.com>
2025-05-13 18:13:30 -0300

    feat: Bump Subtis to v0.7.1

--- packages/api/app.ts
+++ packages/api/app.ts
@@ -41,7 +41,7 @@ export function runApi() {
       openAPISpecs(app, {
         documentation: {
           info: {
-            version: "0.7.0",
+            version: "0.7.1",
             title: "Subtis API",
             description: "API for subtitles and titles",
           },
