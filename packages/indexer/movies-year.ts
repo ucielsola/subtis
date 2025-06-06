f5926bb5677d9b90afca7e4402a08d84f99cb271
lndgalante <lndgalante@gmail.com>
2025-06-06 19:52:43 -0300

    feat: Ignore text file

--- packages/indexer/movies-year.ts
+++ packages/indexer/movies-year.ts
@@ -17,7 +17,7 @@ import { indexMoviesByYear } from "./movies";
 indexMoviesByYear({
   year: 2025,
   isDebugging: false,
-  indexFromPage: 5,
+  indexFromPage: 33,
 });
 
 // saveReleaseGroupsToDb(supabase);
