6eeb651b9520d5c16676be9aef23690d5dbacd02
Leonardo Galante <lndgalante@gmail.com>
2025-06-12 00:07:25 -0300

    [feat] Add start script for image optimizer

--- packages/indexer/top-300.ts
+++ packages/indexer/top-300.ts
@@ -1,7 +1,7 @@
 import { indexMovieByName } from "./movies";
 import top300 from "./top-300-rotten.json";
 
-let lastIndexedMovieName = "Being There";
+let lastIndexedMovieName = "";
 console.log("\n ~ lastIndexedMovieName:", lastIndexedMovieName);
 
 async function indexTop300FromRottenTomatoes() {
